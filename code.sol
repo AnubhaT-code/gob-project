26c1aed38859515274d9b022b02677cb8966937e
AnubhaT-code <82442088+AnubhaT-code@users.noreply.github.com>
2021-07-16 18:46:50 +0530

    Update code.sol

--- code.sol
+++ code.sol
@@ -69,4 +69,64 @@ contract ballot{
         }
         return true;
     }
+    
+     function vote_candidates( address person_voting,uint symbol_voting )public {
+    
+        require(
+            !voters[person_voting].voted,
+            "The voter already voted."
+        );
+    
+        for ( uint i=0 ; i < candcount ; i++ ){
+            
+        if(candidates[candreg[i].symbol].symbol==symbol_voting){
+            voters[person_voting].choice=symbol_voting;
+        candidates[candreg[i].symbol].count=candidates[candreg[i].symbol].count + 1 ;
+        voters[person_voting].voted=true;
+        }
+        }
+
+    }
+    
+     function result_declaration(address ad)public ischairperson  returns (uint,uint ){
+         
+        uint winning_vote_count = 0;
+        uint total_votes=0;
+        uint pos1;
+        uint pos2;
+        for (uint p = 0; p < candcount ; p++) {
+            if (candreg[p].count > winning_vote_count) {
+                winning_vote_count = candreg[p].count;
+                pos1=p;
+            }
+             total_votes+=candreg[p].count;
+        }
+        bool check_winner= ( winning_vote_count > (total_votes-winning_vote_count) ) ?true:false;
+        if(check_winner)
+        {
+           return(pos1,0);
+        }
+        
+        else
+        {
+            winning_vote_count = 0;
+            candreg[pos1].count=0;
+             for (uint q = 0; q < candcount ; q++) {
+            if (candreg[q].count > winning_vote_count) {
+                winning_vote_count = candreg[q].count;
+                pos2=q;
+            }
+        }
+        candreg[pos2].count=0;
+        return(pos1,pos2);
+        //RE-ELECTION
+        }
+     }
+        function re_election_voting(address ad) public ischairperson returns (bool){
+            for(uint i=0;i<votereg.length;i++)
+            voters[votereg[i].id].voted=false;
+              return true;    
+        }
+   
+
 }
